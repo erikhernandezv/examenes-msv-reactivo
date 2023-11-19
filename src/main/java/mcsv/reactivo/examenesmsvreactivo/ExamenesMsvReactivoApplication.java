2b407740d41c9eacdf179e24b310e9732d7544b1
Erik Dario Hernandez Vasquez <erikdhv@gmail.com>
2023-11-19 14:56:30 -0500

    Comit inicial del proyecto

---
+++ src/main/java/mcsv/reactivo/examenesmsvreactivo/ExamenesMsvReactivoApplication.java
@@ -0,0 +1,13 @@
+package mcsv.reactivo.examenesmsvreactivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenesMsvReactivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenesMsvReactivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mcsv/reactivo/examenesmsvreactivo/ExamenesMsvReactivoApplicationTests.java
@@ -0,0 +1,13 @@
+package mcsv.reactivo.examenesmsvreactivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenesMsvReactivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
